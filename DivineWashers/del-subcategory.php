0c43cfe74257733a82cf3c454df51e749aac1c8d
Steven632 <steven.rodriguez18@upr.edu>
2022-04-16 12:02:20 -0400

    template admin
    añadí parte del template de admin. Falta por terminar.

---
+++ DivineWashers/del-subcategory.php
@@ -0,0 +1,19 @@
+<?php                   
+    $sid = $_POST['subcategory-id'];
+    $db = mysqli_connect('server-name', 'user-name', 'password', 'database-name');
+    $query = "DELETE FROM subcategory where sid = '$sid'";
+    $result = mysqli_query($db, $query);
+    if ($result == FALSE) 
+    {
+        die(mysqli_error());
+        echo "Errorbb";
+        exit();
+    }
+    else{
+        
+        echo "Subcategory deleted";
+        $URL="categories.php";
+        echo "<script type='text/javascript'>document.location.href='{$URL}';</script>";
+        echo '<META HTTP-EQUIV="refresh" content="0;URL=' . $URL . '">';
+    }
+?>
