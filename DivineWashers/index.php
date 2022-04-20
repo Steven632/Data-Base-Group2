d388226904c29d646fff975ad455571a1714b685
Gabriel Velazquez Flanegien <gabriel.velazquez4@upr.edu>
2022-04-20 19:36:52 -0400

    revert change
    cambie el cambio que hice

--- DivineWashers/index.php
+++ DivineWashers/index.php
@@ -72,7 +72,7 @@
                             <div class="nav-item dropdown">
                                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">User Account</a>
                                 <div class="dropdown-menu">
-                                    <a href="Divinewashers/login.php" class="dropdown-item">Login/Register</a>
+                                    <a href="login.php" class="dropdown-item">Login/Register</a>
                                 </div>
                             </div>
                         </div>
