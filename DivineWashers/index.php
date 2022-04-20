f6f0291a3bc0d96337e0eac853d862a6ca0d67db
Gabriel Velazquez Flanegien <gabriel.velazquez4@upr.edu>
2022-04-19 22:13:17 -0400

    reverti cambios del index
    lo deje como estaba, comoquiera no me quiere abrir la pagina

--- DivineWashers/index.php
+++ DivineWashers/index.php
@@ -1,12 +1,3 @@
-<!--TRATANDO DE SOLUCIONAR DOWNLOAD EN VEZ DE OPEN
-<?php
-include 'connection.php';
-//require "database.php";
-
-//$costumer = $connection->query("SELECT * FROM Costumer");
-
-?>
-TRATANDO DE SOLUCIONAR DOWNLOAD EN VEZ DE OPEN-->
 <!DOCTYPE html>
 <html lang="en">
     <head>
