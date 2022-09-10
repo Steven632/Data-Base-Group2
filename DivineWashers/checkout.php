976939f15f628ebb8a590df0afce371242dfde06
Steven632 <steven.rodriguez18@upr.edu>
2022-09-10 02:04:37 -0400

    Update checkout.php

--- DivineWashers/checkout.php
+++ DivineWashers/checkout.php
@@ -199,7 +199,7 @@ $r = mysqli_fetch_assoc($res);
                             <div class="nav-item dropdown">
                                 <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">User Account</a>
                                 <div class="dropdown-menu">
-                                    <a href="login.php" class="dropdown-item">Login/Register</a>
+                                    <a  href="login.php" class="dropdown-item">Login/Register</a>
                                 </div>
                             </div>
                         </div>
